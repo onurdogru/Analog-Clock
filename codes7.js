3e0749d2e1a57f3f4ae3d48b407078a97bfbc8bc
Onur Doğru <onurdogruiletisim@gmail.com>
2022-10-06 01:10:19 +0300

    first commit

---
+++ codes7.js
@@ -0,0 +1,20 @@
+/* 8.ADIM : CONSTRUCTOR YAPISI */
+
+const sec = document.querySelector(".secs");
+const min = document.querySelector(".mins");
+const hours = document.querySelector(".hours");
+
+/* 9.ADIM : Bir Fonksiyon yazmamız gereklidir.  */
+
+function tiktak() {
+let second = new Date().getSeconds();
+let minute = new Date().getMinutes();
+let hour = new Date().getHours();
+
+
+    sec.style.transform = `rotate(${180 + (second * 6)}deg)`
+    min.style.transform = `rotate(${180 + (minute * 6)}deg)`
+    hours.style.transform = `rotate(${180 + (hour * 30)}deg)`
+}
+
+setInterval(tiktak, 1000);
